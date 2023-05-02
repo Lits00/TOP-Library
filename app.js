a15f56e6f915527409c2773507da2e6b8fd06a4f
Angelito <litsdionisio@gmail.com>
2023-05-02 15:30:08 +0400

    Created add book modal and book card format

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 let myLibrary = [];
 
+const modalBtn = document.getElementById('modal-btn');
+const modal = document.querySelector('.modal');
+const cancelBtn = document.getElementById('cancel');
+
+modalBtn.addEventListener('click', () => modal.style.display = 'block');
+cancelBtn.addEventListener('click', () => modal.style.display = 'none');
+
+modal.addEventListener('click', (event) => {
+    if(event.target === modal){
+        modal.style.display = 'none';
+    }
+});
+
 function Book(title, author, pages, read) {
     this.title = title;
     this. author = author;
