420a5fd6c10bbadd0f0b6a61474ed8f729de29b9
Angelito <litsdionisio@gmail.com>
2023-05-04 12:09:58 +0400

    Add book card function

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
-let myLibrary = [];
+let Library = [];
 
-const modalBtn = document.getElementById('modal-btn');
+// Inputs
+const title = document.getElementById('title');
+const author = document.getElementById('author');
+const pages = document.getElementById('pages');
+const status = document.getElementById('status');
+
+// Buttons
+const addBookBtn = document.getElementById('add-book');
 const modal = document.querySelector('.modal');
+const submitBtn = document.getElementById('submit');
 const cancelBtn = document.getElementById('cancel');
+const statusBtn = document.querySelector('.status');
+const deleteBtn = document.querySelector('.delete');
 
-modalBtn.addEventListener('click', () => modal.style.display = 'block');
-cancelBtn.addEventListener('click', () => modal.style.display = 'none');
-
-modal.addEventListener('click', (event) => {
-    if(event.target === modal){
-        modal.style.display = 'none';
-    }
-});
-
+// Book constructor
 function Book(title, author, pages, read) {
     this.title = title;
     this. author = author;
@@ -24,5 +26,66 @@ function Book(title, author, pages, read) {
 };
 
 function addBookToLibrary() {
+    createBookCard();
+    closeModal();
+};
 
-}
+function createBookCard() {
+    const bookContainer = document.querySelector('.book-container');
+    const bookCard = document.createElement('div');
+    const bookInfo = document.createElement('div');
+    const bookTitle = document.createElement('p');
+    const bookAuthor = document.createElement('p');
+    const bookPages = document.createElement('p');
+    const bookStatus = document.createElement('button');
+    const deleteBook = document.createElement('button');
+
+    bookCard.classList.add('book-card');
+    bookInfo.classList.add('book-info');
+
+    bookTitle.classList.add('book-title');
+    bookTitle.textContent = "Title: ";
+
+    bookAuthor.classList.add('book-author');
+    bookAuthor.textContent = "Author: ";
+
+    bookPages.classList.add('book-pages');
+    bookPages.textContent = "Pages: ";
+
+    bookStatus.classList.add('btn', 'status');
+    deleteBook.classList.add('btn', 'delete');
+    deleteBook.textContent = "Delete"
+    
+    bookContainer.appendChild(bookCard);
+    bookCard.appendChild(bookInfo);
+    bookInfo.appendChild(bookTitle);
+    bookInfo.appendChild(bookAuthor);
+    bookInfo.appendChild(bookPages);
+    bookCard.appendChild(bookStatus);
+    bookCard.appendChild(deleteBook);
+};
+
+function closeModal(){
+    modal.style.display = 'none';
+};
+
+addBookBtn.addEventListener('click', () => modal.style.display = 'block');
+
+// submitBtn.addEventListener('click', (event) => {
+//     event.preventDefault();
+//     addBookToLibrary();
+// });
+
+// cancelBtn.addEventListener('click', (event) => {
+//     event.preventDefault();
+//     closeModal();
+// });
+
+submitBtn.addEventListener('click', addBookToLibrary);
+
+cancelBtn.addEventListener('click', closeModal);
+
+modal.addEventListener('click', (event) => {
+    if(event.target === modal) closeModal();
+});
+
