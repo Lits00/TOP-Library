5ee1e73774858ce0aeed016e7c0b5e1069e7011f
Angelito Dionisio <litsdionisio@gmail.com>
2023-07-10 14:35:17 +0400

    Code revision and Implementation of localStorage

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-let library = [];
-
 // Inputs
 const title = document.getElementById('title');
 const author = document.getElementById('author');
@@ -14,6 +12,24 @@ const cancelBtn = document.getElementById('cancel');
 const statusBtns = document.querySelectorAll('.status');
 const deleteBtns = document.querySelectorAll('.delete');
 
+class Library {
+    constructor() {
+        this.storage = []
+    }
+
+    addBook(book) {
+        if(!this.bookList(book)) {
+            this.storage.push(book);
+        } else {
+            alert(`The book "${book.title}" already exist!`);
+        }
+    }
+
+    bookList(newBook) {
+       return this.storage.some(book => book.title === newBook.title)
+    }
+}
+
 class Book {
     constructor(title, author, pages, read) {
         this.title = title;
@@ -21,92 +37,127 @@ class Book {
         this.pages = pages;
         this.read = read;
     }
-
+    
     bookInfo() {
         return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
     }
 }
 
-function addBookToLibrary() {
-    createBookCard();
-    closeModal();
-};
+const library = new Library()
 
-function createBookCard() {
-    const bookContainer = document.querySelector('.book-container');
-    const bookCard = document.createElement('div');
-    const bookInfo = document.createElement('div');
-    const pTitle = document.createElement('p');
-    const pAuthor = document.createElement('p');
-    const pPages = document.createElement('p');
-    const spanTitle = document.createElement('span');
-    const spanAuthor = document.createElement('span');
-    const spanPages = document.createElement('span');
-    const bookStatus = document.createElement('button');
-    const deleteBook = document.createElement('button');
-    
+const addBookToLibrary = () => {
+
     const inputTitle = title.value;
     const inputAuthor = author.value;
     const inputPages = pages.value;
     const read = isRead.checked ? "Read" : "Not read";
 
     const book = new Book(inputTitle, inputAuthor, inputPages, read);
-    library.push(book);
+    library.addBook(book)
 
-    bookCard.classList.add('book-card');
-    bookInfo.classList.add('book-info');
-
-    pTitle.classList.add('book-title');
-    pTitle.textContent = "Title: ";
-    spanTitle.classList.add('input-title');
-    spanTitle.textContent = inputTitle;
-
-    pAuthor.classList.add('book-author');
-    pAuthor.textContent = "Author: ";
-    spanAuthor.classList.add('input-author');
-    spanAuthor.textContent = inputAuthor;
-
-    pPages.classList.add('book-pages');
-    pPages.textContent = "Pages: ";
-    spanPages.classList.add('input-pages');
-    spanPages.textContent = inputPages;
-
-    bookStatus.classList.add('btn', 'status');
-    bookStatus.textContent = read;
-    deleteBook.classList.add('btn', 'delete');
-    deleteBook.textContent = "Delete";
-    
-    bookContainer.appendChild(bookCard);
-    bookCard.appendChild(bookInfo);
-    bookInfo.appendChild(pTitle);
-    pTitle.appendChild(spanTitle);
-    bookInfo.appendChild(pAuthor);
-    pAuthor.appendChild(spanAuthor);
-    bookInfo.appendChild(pPages);
-    pPages.appendChild(spanPages);
-    bookCard.appendChild(bookStatus);
-    bookCard.appendChild(deleteBook);
+    saveToStorage();
+    resetLibrary();
+    createBookCard();
 };
 
-// Updates the book status from the array
-function updateBookStatus(bookTitle, bookStatus){
-    const i = library.findIndex((book) => book.title === bookTitle);
-    if(i !== -1) library[i].read = bookStatus;
+const createBookCard = () => {
+    retrieveBooks();
+    library.storage.forEach( book => {
+        const bookContainer = document.querySelector('.book-container');
+        const bookCard = document.createElement('div');
+        const bookInfo = document.createElement('div');
+        const pTitle = document.createElement('p');
+        const pAuthor = document.createElement('p');
+        const pPages = document.createElement('p');
+        const spanTitle = document.createElement('span');
+        const spanAuthor = document.createElement('span');
+        const spanPages = document.createElement('span');
+        const bookStatus = document.createElement('button');
+        const deleteBook = document.createElement('button');
+
+        const bookTitle = book.title;
+        const bookAuthor = book.author;
+        const bookPages = book.pages;
+        const read = book.read;
+
+        bookCard.classList.add('book-card');
+        bookInfo.classList.add('book-info');
+
+        pTitle.classList.add('book-title');
+        pTitle.textContent = "Title: ";
+        spanTitle.classList.add('input-title');
+        spanTitle.textContent = bookTitle;
+
+        pAuthor.classList.add('book-author');
+        pAuthor.textContent = "Author: ";
+        spanAuthor.classList.add('input-author');
+        spanAuthor.textContent = bookAuthor;
+
+        pPages.classList.add('book-pages');
+        pPages.textContent = "Pages: ";
+        spanPages.classList.add('input-pages');
+        spanPages.textContent = bookPages;
+
+        bookStatus.classList.add('btn', 'status');
+        bookStatus.textContent = read;
+        deleteBook.classList.add('btn', 'delete');
+        deleteBook.textContent = "Delete";
+        
+        bookContainer.appendChild(bookCard);
+        bookCard.appendChild(bookInfo);
+        bookInfo.appendChild(pTitle);
+        pTitle.appendChild(spanTitle);
+        bookInfo.appendChild(pAuthor);
+        pAuthor.appendChild(spanAuthor);
+        bookInfo.appendChild(pPages);
+        pPages.appendChild(spanPages);
+        bookCard.appendChild(bookStatus);
+        bookCard.appendChild(deleteBook);
+    })
 };
 
-function deleteBook(bookTitle){
-    const i = library.findIndex((book) => book.title === bookTitle);
-    if(i !== -1) library.splice(i, 1);
+const retrieveBooks = () => {
+    const books = JSON.parse(localStorage.getItem('Library'))
+    if(books) {
+        library.storage = books.map(book => convertJSON(book))
+    } else {
+        library.storage = []
+    }
 }
 
-function resetModal() {
+const convertJSON = (book) => {
+    return new Book(book.title, book.author, book.pages, book.read)
+}
+
+const saveToStorage = () => {
+    localStorage.setItem('Library', JSON.stringify(library.storage))
+}
+
+const updateBook = (bookTitle, bookStatus) => {
+    const bookIndex = library.storage.findIndex((book) => book.title === bookTitle);
+    if(bookIndex !== -1) library.storage[bookIndex].read = bookStatus;
+    saveToStorage();
+};
+
+const deleteBook = (bookTitle) => {
+    const bookIndex = library.storage.findIndex((book) => book.title === bookTitle);
+    if(bookIndex !== -1) library.storage.splice(bookIndex, 1);
+    saveToStorage();
+}
+
+const resetLibrary = () => {
+    const container = document.querySelector('.book-container');
+    container.innerHTML = ''
+}
+
+const resetModal = () => {
     title.value = '';
     author.value = '';
     pages.value = '';
     isRead.checked = false; 
 };
 
-function closeModal() {
+const closeModal = () => {
     modal.style.display = 'none';
 };
 
@@ -118,6 +169,7 @@ addBookBtn.addEventListener('click', () => {
 submitBtn.addEventListener('click', (event) => {
     if(title.value === '' || author.value === '' || pages.value === '') return
     event.preventDefault();
+    closeModal();
     addBookToLibrary();
 });
 
@@ -130,15 +182,19 @@ modal.addEventListener('click', (event) => {
     if(event.target === modal) closeModal();
 });
 
+document.addEventListener('keydown', function(event) {
+    if (event.key === "Escape") closeModal();
+});
+
 // Book Status and Delete func
 document.addEventListener('click', (event) => {
     // Updates the status button from book card
     if(event.target.classList.contains('status')) {
-      const bookCard = event.target.closest('.book-card');
-      const bookTitle = bookCard.querySelector('.input-title').textContent;
-      const bookStatus = (event.target.textContent === 'Read') ? 'Not read' : 'Read';
-      event.target.textContent = bookStatus;
-      updateBookStatus(bookTitle, bookStatus);
+        const bookCard = event.target.closest('.book-card');
+        const bookTitle = bookCard.querySelector('.input-title').textContent;
+        const bookStatus = (event.target.textContent === 'Read') ? 'Not read' : 'Read';
+        event.target.textContent = bookStatus;
+        updateBook(bookTitle, bookStatus)
     }
 
     // Deletes the book from the list
@@ -149,3 +205,5 @@ document.addEventListener('click', (event) => {
         deleteBook(bookTitle);
     }
 });
+
+createBookCard();
